71bd1cec0699f31de5232f53b1a4cec5a52e8383
Luca Guidi <me@lucaguidi.com>
2019-01-24 15:13:08 +0100

    Initial mess

---
+++ lib/hanami/scaffold.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Hanami
+  module Scaffold
+    require "hanami/scaffold/version"
+    require "hanami/scaffold/cli"
+  end
+end
---
+++ lib/hanami/scaffold/cli.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require "shellwords"
+require "hanami/utils/string"
+require "hanami/cli/commands"
+
+module Hanami
+  module Scaffold
+    module CLI
+      class Command < Hanami::CLI::Commands::Command
+        desc "Generate scaffold"
+
+        argument :app,   "The app name"
+        argument :model, "The model name"
+
+        # rubocop:disable Metrics/MethodLength
+        def call(app, model, **)
+          app      = Shellwords.escape(app)
+          singular = Shellwords.escape(model)
+          plural   = Utils::String.pluralize(singular)
+
+          exec "bundle exec hanami generate model #{singular}"
+
+          exec "bundle exec hanami generate action #{app} #{plural}#index --url=/#{plural}"
+          exec "bundle exec hanami generate action #{app} #{plural}#show --url=/#{plural}"
+          exec "bundle exec hanami generate action #{app} #{plural}#new --url=/#{plural}/new"
+          exec "bundle exec hanami generate action #{app} #{plural}#create --url=/#{plural}"
+          exec "bundle exec hanami generate action #{app} #{plural}#edit --url=/#{plural}/edit"
+          exec "bundle exec hanami generate action #{app} #{plural}#update --url=/#{plural}"
+          exec "bundle exec hanami generate action #{app} #{plural}#destroy --url=/#{plural}"
+        end
+        # rubocop:enable Metrics/MethodLength
+      end
+    end
+  end
+end
